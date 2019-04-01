7a32b5c3751d88745de4f68a606d3048bbeea6ab
tjabbe <martin.goutry@gmail.com>
2019-04-01 23:51:16 +0200

    Add analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,6 +29,17 @@ module.exports = {
         icon: `src/images/joyjang-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-plugin-gtag`,
+      options: {
+        // your google analytics tracking id
+        trackingId: `UA-137473136-1`,
+        // Puts tracking script in the head instead of the body
+        head: true,
+        // enable ip anonymization
+        anonymize: true,
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // 'gatsby-plugin-offline',
